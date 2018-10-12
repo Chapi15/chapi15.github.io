094d35c35a1d79b1bba46b6a65b9e915881aa447
Alfonso Rodríguez Ongay <alfonso.ro.96@gmail.com>
2018-10-12 11:15:45 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,15 @@
 //Test for Non-obstrusive JS
 
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
+//var myHeading = document.querySelector('h1');
+//myHeading.textContent = 'Hello World!';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/nyanCat.jpeg') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/nyanCat.gif');
+    }
+}
